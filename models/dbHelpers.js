e2eabbcea3073eb86aca0087856fc20997a30a4c
Marcelo Casamassa <marcelocasamassa@gmaile.com>
2021-03-22 16:42:10 -0300

    writing more queries for database

--- models/dbHelpers.js
+++ models/dbHelpers.js
@@ -5,14 +5,28 @@ const db = knex(config.development)
 
 async function add(lesson) {
     const [id] = await db('lessons').insert(lesson)
-    return id
+    //return id
 }
 
 function find() {
     return db('lessons')
 }
 
+function findById(id) {
+    return db('lessons')
+        .where({ id: id })
+        .first()
+}
+
+function remove(id) {
+    return db('lessons')
+        .where({ id })
+        .del()
+}
+
 module.exports = { 
     add, 
-    find
+    find,
+    findById,
+    remove
 }
