667de6b3776c32f02e6f6c238628a12cd3095c31
Marcelo Casamassa <marcelocasamassa@gmaile.com>
2021-03-23 14:54:52 -0300

    added production object to knexfile

--- knexfile.js
+++ knexfile.js
@@ -12,6 +12,15 @@ module.exports = {
     }
   },
   production: {
-    
+    client: 'pg',
+    connection: process.env.DATABASE_URL,
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tablename: 'knex_migrations',
+      directory: './migrations',
+    }
   }
 };
