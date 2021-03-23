fdac05ea93e0fb1d159241f8cbfc3f0c61649edb
Marcelo Casamassa <marcelocasamassa@gmaile.com>
2021-03-23 16:18:48 -0300

    another try to fiz SSL error

--- knexfile.js
+++ knexfile.js
@@ -13,8 +13,10 @@ module.exports = {
   },
   production: {
     client: 'pg',
-    connection: process.env.DATABASE_URL,
-    ssl: true,
+    connection: {
+      connectionString: process.env.DATABASE_URL,
+      ssl: true
+    },
     pool: {
       min: 2,
       max: 10
